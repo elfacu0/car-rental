36da05834189abaa481268ecf9cb2fc306831d4d
k- <mahat.mejor@gmail.com>
2020-09-30 10:36:51 -0500

    jest is installed

---
+++ src/module/__tests__/abstractController.test.js
@@ -0,0 +1,15 @@
+const AbstractController = require('../abstractController');
+const AbstractControllerError = require('../error/abstractControllerError');
+
+test('No se puede crear una nueva instancia de un AbstractController directamente', () => {
+    try {
+        new AbstractController();
+    } catch (e) {
+        expect(e).toBeInstanceOf(AbstractControllerError);
+    }
+});
+
+test('Se puede crear una nueva instancia de una clase que hereda de AbstractController', () => {
+    const ConcreteController = class extends AbstractController {};
+    expect(new ConcreteController()).toBeInstanceOf(AbstractController);
+});
